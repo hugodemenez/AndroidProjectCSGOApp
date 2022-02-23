ef05ae2cf8ccb3b8b33a5bae26fd26b1f98fafde
Hugo Demenez <hdemenez@hotmail.fr>
2022-02-23 16:19:38 +0100

    testing commit

--- app/src/main/java/com/example/csgoapp/MainActivity.java
+++ app/src/main/java/com/example/csgoapp/MainActivity.java
@@ -12,8 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText emailAdress;
-    private Button loginButton;
+
 
 
     @Override
@@ -21,19 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginlayout);
 
-        loginButton = (Button) findViewById(R.id.buttonLogin);
-        emailAdress = (EditText) findViewById(R.id.editTextEmail);
-
-        Log.e("email : ",emailAdress.getText().toString());
     }
 
 
-    public void onClick(View view) {
-        if(TextUtils.isEmpty(emailAdress.getText())){
-            Toast.makeText(this,"emailAdress empty",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-    }
 
 }
