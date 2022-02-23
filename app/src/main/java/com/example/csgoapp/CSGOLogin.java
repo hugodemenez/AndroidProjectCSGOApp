4c3376d8c68ae5f5a3dff641f39ad2becf7e8fe1
Hugo Demenez <hdemenez@hotmail.fr>
2022-02-23 17:42:53 +0100

    adding firebase

--- app/src/main/java/com/example/csgoapp/CSGOLogin.java
+++ app/src/main/java/com/example/csgoapp/CSGOLogin.java
@@ -1,23 +1,29 @@
 package com.example.csgoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class CSGOLogin extends AppCompatActivity {
 
     private EditText emailAdress;
+    private EditText password;
     private FirebaseAuth mAuth;
 
     @Override
@@ -25,6 +31,7 @@ public class CSGOLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginlayout);
         emailAdress = (EditText) findViewById(R.id.editTextEmail);
+        password = (EditText) findViewById(R.id.editTextPassword);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -34,24 +41,38 @@ public class CSGOLogin extends AppCompatActivity {
 
     public void onClickLogin(View view) {
 
-
-        if(TextUtils.isEmpty(emailAdress.getText())){
-            Toast.makeText(this,"Type your mail !",Toast.LENGTH_LONG).show();
-            Log.e("email : ",emailAdress.getText().toString());
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailAdress.getText()).matches() || TextUtils.isEmpty(emailAdress.getText())){
+            emailAdress.setError("Please provide a valid address");
+            emailAdress.requestFocus();
+            return;
         }
-
         else{
-
+            if(password.length() < 6){
+                password.setError("Enter a password longer than 6 characters");
+                password.requestFocus();
+                return;
+            }
         }
+
+        mAuth.createUserWithEmailAndPassword(emailAdress.getText().toString(),password.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            startActivity(getIntent(emailAdress.getText().toString(),CSGOWelcome.class));
+                        }
+                    }
+                }
+        );
     }
 
     public void onClickSignin(View view) {
-        startActivity(getIntent(emailAdress.getText().toString()));
+        startActivity(getIntent(emailAdress.getText().toString(),CSGOSign.class));
     }
 
 
-    private Intent getIntent(String emailAdress){
-        Intent intent = new Intent(this, CSGOSign.class);
+    private Intent getIntent(String emailAdress,Class page){
+        Intent intent = new Intent(this, page);
         final Bundle extras = new Bundle();
         extras.putString("email",emailAdress);
         intent.putExtras(extras);
---
+++ app/src/main/java/com/example/csgoapp/CSGOWelcome.java
@@ -0,0 +1,19 @@
+package com.example.csgoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CSGOWelcome extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcomelayout);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+}
