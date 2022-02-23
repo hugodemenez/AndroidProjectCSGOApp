c7df1eb9705e6e2f1acdd574d6aa2f40b7f50dd7
Hugo Demenez <hdemenez@hotmail.fr>
2022-02-23 15:33:36 +0100

    testing commit

--- app/src/main/java/com/example/csgoapp/MainActivity.java
+++ app/src/main/java/com/example/csgoapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.csgoapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.loginlayout);
+
+
+
     }
+
+
 }
