5d07441ebb5aa117ed2a955fe0f406f356edb2e0
Hugo Demenez <hdemenez@hotmail.fr>
2022-02-27 20:28:43 +0100

    trying to get data from firebase

--- app/src/main/java/com/example/csgoapp/CSGOChoose.java
+++ app/src/main/java/com/example/csgoapp/CSGOChoose.java
@@ -1,17 +1,27 @@
 package com.example.csgoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,7 +52,9 @@ public class CSGOChoose extends AppCompatActivity {
         adapterGrenade.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinnerGrenade.setAdapter(adapterGrenade);
+        getDataFromDatabase();
 
+        CustomAdapter recyclerView = new CustomAdapter(null);
     }
 
     public void onClickBackButton(View view) {
@@ -64,5 +76,87 @@ public class CSGOChoose extends AppCompatActivity {
             put("grenade",spinnerGrenade.getSelectedItem().toString());
         }};
         new CSGOActivityStarter(this,extras,addcontentlayout.class);
+    }
+
+    public Map<String, Object> getDataFromDatabase(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> content;
+        db.collection("contents")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // Create a new user with a first and last name
+                                Map<String, Object> content = new HashMap<>();
+                                content.put("map", document.getData().get("map"));
+                                content.put("grenade", document.getData().get("grenade"));
+                                content.put("side", document.getData().get("side"));
+                                content.put("contentTitle", document.getData().get("contentTitle"));
+                                content.put("videoUrl", document.getData().get("videoUrl"));
+                                content.put("image1Url", document.getData().get("image1Url"));
+                                content.put("image2Url", document.getData().get("image2Url"));
+                                Log.d("Database Get", document.getId() + " => " + content);
+
+                            }
+                        } else {
+                            Log.d("Database Get", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+        return null;
+    }
+
+    public void populateDatabase(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference cities = db.collection("cities");
+
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("name", "San Francisco");
+        data1.put("state", "CA");
+        data1.put("country", "USA");
+        data1.put("capital", false);
+        data1.put("population", 860000);
+        data1.put("regions", Arrays.asList("west_coast", "norcal"));
+        cities.document("SF").set(data1);
+
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("name", "Los Angeles");
+        data2.put("state", "CA");
+        data2.put("country", "USA");
+        data2.put("capital", false);
+        data2.put("population", 3900000);
+        data2.put("regions", Arrays.asList("west_coast", "socal"));
+        cities.document("LA").set(data2);
+
+        Map<String, Object> data3 = new HashMap<>();
+        data3.put("name", "Washington D.C.");
+        data3.put("state", null);
+        data3.put("country", "USA");
+        data3.put("capital", true);
+        data3.put("population", 680000);
+        data3.put("regions", Arrays.asList("east_coast"));
+        cities.document("DC").set(data3);
+
+        Map<String, Object> data4 = new HashMap<>();
+        data4.put("name", "Tokyo");
+        data4.put("state", null);
+        data4.put("country", "Japan");
+        data4.put("capital", true);
+        data4.put("population", 9000000);
+        data4.put("regions", Arrays.asList("kanto", "honshu"));
+        cities.document("TOK").set(data4);
+
+        Map<String, Object> data5 = new HashMap<>();
+        data5.put("name", "Beijing");
+        data5.put("state", null);
+        data5.put("country", "China");
+        data5.put("capital", true);
+        data5.put("population", 21500000);
+        data5.put("regions", Arrays.asList("jingjinji", "hebei"));
+        cities.document("BJ").set(data5);
     }
 }
