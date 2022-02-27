8b90b41161596b9255e0f8f36098189effc3ea11
Hugo Demenez <hdemenez@hotmail.fr>
2022-02-27 19:39:56 +0100

    adding firebase content add

--- app/src/main/java/com/example/csgoapp/CSGOChoose.java
+++ app/src/main/java/com/example/csgoapp/CSGOChoose.java
@@ -6,18 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CSGOChoose extends AppCompatActivity {
+    private Spinner spinnerSide;
+    private Spinner spinnerGrenade;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chooselayout);
 
-        Spinner spinnerSide = (Spinner) findViewById(R.id.spinnerSide);
+        spinnerSide = (Spinner) findViewById(R.id.spinnerSide);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.side, android.R.layout.simple_spinner_item);
@@ -26,7 +34,7 @@ public class CSGOChoose extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinnerSide.setAdapter(adapter);
 
-        Spinner spinnerGrenade = (Spinner) findViewById(R.id.spinnerGrenade);
+        spinnerGrenade = (Spinner) findViewById(R.id.spinnerGrenade);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapterGrenade = ArrayAdapter.createFromResource(this,
                 R.array.grenadeType, android.R.layout.simple_spinner_item);
@@ -34,6 +42,7 @@ public class CSGOChoose extends AppCompatActivity {
         adapterGrenade.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinnerGrenade.setAdapter(adapterGrenade);
+
     }
 
     public void onClickBackButton(View view) {
@@ -47,5 +56,13 @@ public class CSGOChoose extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         finish();
+    }
+
+    public void onClickAddContent(View view) {
+        Map<String,String> extras = new HashMap<String,String>() {{
+            put("side",spinnerSide.getSelectedItem().toString());
+            put("grenade",spinnerGrenade.getSelectedItem().toString());
+        }};
+        new CSGOActivityStarter(this,extras,addcontentlayout.class);
     }
 }
---
+++ app/src/main/java/com/example/csgoapp/addcontentlayout.java
@@ -0,0 +1,75 @@
+package com.example.csgoapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class addcontentlayout extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String grenade = "";
+    private String side = "";
+    private EditText contentTitle;
+    private EditText videoUrl;
+    private EditText image1Url;
+    private EditText image2Url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addcontentlayout);
+        final Intent intent = getIntent();
+        if(null!=intent){
+            final Bundle extras = intent.getExtras();
+            if(null!=extras){
+                grenade = extras.getString("grenade");
+                side = extras.getString("side");
+            }
+        }
+
+    }
+
+    public void addContent(View view){
+        videoUrl = findViewById(R.id.editTextVideoUrl);
+        image1Url = findViewById(R.id.editTextImage1Url);
+        image2Url = findViewById(R.id.editTextImage2Url);
+        contentTitle = findViewById(R.id.editTextContentTitle);
+        // Create a new user with a first and last name
+        Map<String, Object> content = new HashMap<>();
+        content.put("map", "Inferno");
+        content.put("grenade", grenade);
+        content.put("side", side);
+        content.put("contentTitle", contentTitle.getText().toString());
+        content.put("videoUrl", videoUrl.getText().toString());
+        content.put("image1Url", image1Url.getText().toString());
+        content.put("image2Url", image2Url.getText().toString());
+
+        // Add a new document with a generated ID
+        db.collection("contents")
+                .add(content)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("OK", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("OK", "Error adding document", e);
+                    }
+                });
+    }
+}
