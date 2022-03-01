a98fa942b7a651b0fcd400eee9fad0fda58f88a5
Kiesky <kevin.tobal@hotmail.fr>
2022-03-01 19:55:47 +0100

    Ajout dans la base de donné avec "addDB"

--- app/src/main/java/com/example/csgoapp/MainActivity.java
+++ app/src/main/java/com/example/csgoapp/MainActivity.java
@@ -23,14 +23,159 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        addToDb( "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        ""
+        addToDb( "Inferno",
+        "Smoke",
+        "Terrorists",
+        "Banana to deep CT",
+        "https://www.youtube.com/watch?v=qwKYv0fMX5I",
+        "https://i.ibb.co/479g1Cp/20220227174205-1.jpg",
+        "https://i.ibb.co/Fs2y0Sf/20220301190156-1.jpg"
         );
+        addToDb( "Inferno",
+                "Smoke",
+                "Terrorists",
+                "Banana to coffin",
+                "https://www.youtube.com/watch?v=pZBR6CXfCVQ",
+                "https://i.ibb.co/GFhdvty/20220227173904-1.jpg",
+                "https://i.ibb.co/z6XpdrK/20220227173849-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "Terrorists",
+                "Banana cross pool",
+                "https://www.youtube.com/watch?v=l3GK8czdSv8",
+                "https://i.ibb.co/n6G3DvH/20220227174058-1.jpg",
+                "https://i.ibb.co/chYwGhJ/20220227174118-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "Terrorists",
+                "Mid to plane",
+                "https://www.youtube.com/watch?v=mapt01PeIqM",
+                "https://i.ibb.co/jRyq0QR/20220227180245-1.jpg",
+                "https://i.ibb.co/xXQzHpy/20220227184602-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "Terrorists",
+                "Pit to plane",
+                "https://www.youtube.com/watch?v=WuloTQe7Nd0",
+                "https://i.ibb.co/6Fc0qNH/20220227180421-1.jpg",
+                "https://i.ibb.co/GR81xCq/20220227180351-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "Terrorists",
+                "Mid to long",
+                "https://www.youtube.com/watch?v=d4VSBGPoE5E",
+                "https://i.ibb.co/MCTnqX1/20220227180224-1.jpg",
+                "https://i.ibb.co/4mqsBKk/20220227180125-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "Terrorists",
+                "Mid to short",
+                "https://www.youtube.com/watch?v=K4_x0S-pCmU",
+                "https://i.ibb.co/MCTnqX1/20220227180224-1.jpg",
+                "https://i.ibb.co/2vNb5XJ/20220227180123-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "Terrorists",
+                "Appart to balcony",
+                "https://www.youtube.com/watch?v=5BcXp_dsW6w",
+                "https://i.ibb.co/b3t38Xk/20220227180713-1.jpg",
+                "https://i.ibb.co/Pxsn9F7/20220227180619-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Molotov",
+                "Terrorists",
+                "Second mid to boost short",
+                "https://www.youtube.com/watch?v=ZCuduQaMLw0",
+                "https://i.ibb.co/V9Ng2v8/20220227180948-1.jpg",
+                "https://i.ibb.co/cJRcmvC/20220227181018-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Molotov",
+                "Terrorists",
+                "Coner long",
+                "https://www.youtube.com/watch?v=fkUcuFWRRzI",
+                "https://i.ibb.co/1bTSs26/20220227184349-1.jpg",
+                "https://i.ibb.co/0jBJCfR/20220227180800-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Molotov",
+                "Terrorists",
+                "Car to new box",
+                "https://www.youtube.com/watch?v=H42BCUodvwc",
+                "https://i.ibb.co/D1mNZQT/20220227181109-1.jpg",
+                "https://i.ibb.co/hX4qRRd/20220227181404-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Molotov",
+                "Terrorists",
+                "B sandwich",
+                "https://www.youtube.com/watch?v=EqMZvwozzOE",
+                "https://i.ibb.co/dfgNCNB/20220227181623-1.jpg",
+                "https://i.ibb.co/mqBDbkH/20220227181647-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Molotov",
+                "Terrorists",
+                "B Dark",
+                "https://www.youtube.com/watch?v=1j3dDdrbl-w",
+                "https://i.ibb.co/C5RbYbh/20220227181424-1.jpg",
+                "https://i.ibb.co/7Y946X5/20220227181459-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "CounterTerrorists",
+                "Deep banana",
+                "https://www.youtube.com/watch?v=Zko5STitbzo",
+                "https://i.ibb.co/KG2zCdK/20220227181734-1.jpg",
+                "https://i.ibb.co/MgbBz14/20220227181758-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "CounterTerrorists",
+                "Half wall banana",
+                "https://www.youtube.com/watch?v=9ht3tzS-2sE",
+                "https://i.ibb.co/cx3R17S/20220227181916-1.jpg",
+                "https://i.ibb.co/CmcQFf9/20220227182118-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "CounterTerrorists",
+                "Cross B site",
+                "https://www.youtube.com/watch?v=LUokz5ue8ho",
+                "https://i.ibb.co/yQKcCNf/20220227182216-1.jpg",
+                "https://i.ibb.co/cv3xSzG/20220301191821-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "CounterTerrorists",
+                "Retake A short",
+                "https://www.youtube.com/watch?v=MQTvyYSxJDs",
+                "https://i.ibb.co/hF3Gyp7/20220227182546-1.jpg",
+                "https://i.ibb.co/pZ8dLwC/20220227182523-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "CounterTerrorists",
+                "Mid from CT",
+                "https://www.youtube.com/watch?v=JzT4sy7ipYQ",
+                "https://i.ibb.co/V3449Ck/20220227182441-1.jpg",
+                "https://i.ibb.co/N7pZBrL/20220227182420-1.jpg"
+        );
+        addToDb( "Inferno",
+                "Smoke",
+                "CounterTerrorists",
+                "Retake apartment",
+                "https://www.youtube.com/watch?v=LQMNpAPUllU",
+                "https://i.ibb.co/W3NxKqd/20220227182623-1.jpg",
+                "https://i.ibb.co/WHYfR2G/20220227182641-1.jpg"
+        );
+
 
 
 
